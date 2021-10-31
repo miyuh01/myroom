58bee76a3c41d1bd011983ca2f529cba02ffb2b4
miyuh01 <83940101+miyuh01@users.noreply.github.com>
2021-10-31 17:09:17 +0900

    Delete works/demosite directory

--- works/demosite/野菜お届け便/javascript/accordion.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-  $('.js-faq__item__link').each(function(){
-    $(this).on('click',function(){
-      $("+.faq__contents__menu",this).slideToggle();
-      return false ;
-   });
-  });
-});
