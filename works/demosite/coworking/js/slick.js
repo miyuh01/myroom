bf1424ed68dd4ec738d2883b132c6fbd8f91bd9b
miyuh01 <83940101+miyuh01@users.noreply.github.com>
2021-12-09 21:53:23 +0900

    Update slick.js

--- works/demosite/coworking/js/slick.js
+++ works/demosite/coworking/js/slick.js
@@ -1,8 +1,9 @@
-$(document).on('ready', function() {
-  $(".slider").slick({
-    dots: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  });
+$('.slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true
 });
+        
